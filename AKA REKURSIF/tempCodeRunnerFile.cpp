613b61a84601226265d552cbb2b82d4443e7fc09
dianakartikaa <dianakartikaa17@gmail.com>
2024-12-23 12:58:02 +0700

    .

--- AKA REKURSIF/tempCodeRunnerFile.cpp
+++ AKA REKURSIF/tempCodeRunnerFile.cpp
@@ -1,2 +1,6 @@
-int N = 3;
-    int A[] = {1, 3, 5};
+// int N = 4;
+    // int A[] = {1, 3, 5, 4};
+
+    // bool foundEven = false;
+    // // Memulai rekursi dari index N-1
+    // findEvenNumbers(A, N-1, foundEven);
